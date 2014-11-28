f531b9f6ff0338bf04408440d5eae2d0b025f5d1
whosAle <alehomora@gmail.com>
2014-11-28 17:22:56 -0500

    Sample Code

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.getElementsByTagName("li")[0].addEventListener("click",function(){
+	if(!this.parentNode.getAttribute("class")){
+		this.parentNode.setAttribute("class","slideDown");
+	}
+	else{
+		this.parentNode.removeAttribute("class");
+	}
+});
+
+$('.menu').click(function() {
+	$('li').show();
+
+});
